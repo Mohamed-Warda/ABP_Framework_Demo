72e56e882807d97e00046fd9beaac787941be9a5
Mohamed Warda <mowarda.eg@gmail.com>
2025-04-18 18:33:57 +0200

    Add Categories service using ABP's CrudAppService base class

---
+++ src/ABPDemo.Application.Contracts/Categories/Contracts/ICategoriesAppService.cs
@@ -0,0 +1,14 @@
+﻿using ABPDemo.Categories.Dtos;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace ABPDemo.Categories.Contracts;
+
+// 'ICrudAppService' is a built-in ABP base Interface that provides standard CRUD operations
+public interface ICategoriesAppService : ICrudAppService<
+    CategoryDto,                    // The DTO used to return category data to the client
+    int,                            // The type of the primary key (ID) of the Category entity
+    PagedAndSortedResultRequestDto, // Used to request paged and sorted lists of categories
+    CreateUpdateCategoryDto>         // The DTO used to create or update a category
+{
+}
---
+++ src/ABPDemo.Application/Categories/CategoriesAppService.cs
@@ -0,0 +1,22 @@
+﻿using ABPDemo.Categories.Contracts;
+using ABPDemo.Categories.Dtos;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace ABPDemo.Categories;
+
+// 'CrudAppService' is a built-in ABP base class that provides standard CRUD operations
+public class CategoriesAppService :
+    CrudAppService<Category,               // The Entity type
+        CategoryDto,            // The DTO used to return data
+        int,                    // The type of the primary key
+        PagedAndSortedResultRequestDto, // Used for pagination and sorting
+        CreateUpdateCategoryDto>,       // The DTO used to create or update a category
+    ICategoriesAppService                  // Interface this service implements
+{
+    public CategoriesAppService(IRepository<Category, int> repository)
+        : base(repository)
+    {
+    }
+}
