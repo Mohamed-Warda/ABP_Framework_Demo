255c5f93a0cbaea9ce405b660bc614db4e27173b
Mohamed Warda <mowarda.eg@gmail.com>
2025-04-18 03:51:17 +0200

    Add Database Seeder

---
+++ src/ABPDemo.Domain/Seeders/CategoryDataSeeder.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ABPDemo.Categories;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+
+namespace ABPDemo.Seeders;
+
+// 'IDataSeedContributor' is an interface used for seeding data into the database.
+// Running the DB Migrator project will trigger the execution of this seed method.
+public class CategoryDataSeeder : IDataSeedContributor, ITransientDependency
+{
+    private readonly IRepository<Category, int> categoriesRepository;
+
+    public CategoryDataSeeder(IRepository<Category, int> categoriesRepository)
+    {
+        this.categoriesRepository = categoriesRepository;
+    }
+
+    public Task SeedAsync(DataSeedContext context)
+    {
+        var categories = new List<Category>()
+        {
+            new Category(id: 1,
+                nameAr:"أطعمة ومشروبات",
+                nameEn: "Food & Drinks",
+                descriptionAr:"جميع أنواع الأطعمة والمشروبات",
+                descriptionEn: "All food and drink categories"),
+            new Category(id: 2,
+                nameAr:"مواد تنظيف",
+                nameEn: "Detergents",
+                descriptionAr:"المنظفات بأنواعها",
+                descriptionEn: "all materials used for cleaning"),
+            new Category(id: 3,
+                nameAr:"عطور",
+                nameEn: "Fragrances",
+                descriptionAr:"العطور بأنواعها",
+                descriptionEn: "all perfumes and its sub-categories"),
+            new Category(id: 4,
+                nameAr:"بلاستيك",
+                nameEn: "Plastic",
+                descriptionAr:"البلاستيك القابل للتدوير والغير قابل للتدوير",
+                descriptionEn: "all reusable and non-reusable plastic materials"),
+        };
+
+        return this.categoriesRepository.InsertManyAsync(categories);
+    }
+}
