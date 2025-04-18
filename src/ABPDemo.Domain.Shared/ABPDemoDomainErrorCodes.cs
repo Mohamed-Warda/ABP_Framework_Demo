ad0f747a8092f9134972371525edfc2786c6e484
Mohamed Warda <mowarda.eg@gmail.com>
2025-04-18 18:36:47 +0200

    Add Product Business Exception with custom error codes

--- src/ABPDemo.Domain.Shared/ABPDemoDomainErrorCodes.cs
+++ src/ABPDemo.Domain.Shared/ABPDemoDomainErrorCodes.cs
@@ -3,4 +3,5 @@
 public static class ABPDemoDomainErrorCodes
 {
     /* You can add your business exception error codes here, as constants */
+    public const string PRODUCT_NOT_FOUND = "STORE:PRODUCTS:0001";
 }
---
+++ src/ABPDemo.Domain/Products/DomainExceptions/ProductNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using Volo.Abp;
+
+namespace ABPDemo.Products.DomainExceptions;
+
+// 'BusinessException' is an ABP class that helps create domain-specific exceptions
+public class ProductNotFoundException: BusinessException
+{
+    public ProductNotFoundException(int id) : base(ABPDemoDomainErrorCodes.PRODUCT_NOT_FOUND)
+    {
+        WithData("id", id);        
+    }
+}
