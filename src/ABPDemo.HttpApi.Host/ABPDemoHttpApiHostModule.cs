7a5e4379c02b65d7fd387f9dd42996b94514c322
Mohamed Warda <mowarda.eg@gmail.com>
2025-04-18 18:41:33 +0200

    Hide ABP end points from swagger UI

--- src/ABPDemo.HttpApi.Host/ABPDemoHttpApiHostModule.cs
+++ src/ABPDemo.HttpApi.Host/ABPDemoHttpApiHostModule.cs
@@ -192,6 +192,7 @@ public class ABPDemoHttpApiHostModule : AbpModule
                 options.SwaggerDoc("v1", new OpenApiInfo { Title = "ABPDemo API", Version = "v1" });
                 options.DocInclusionPredicate((docName, description) => true);
                 options.CustomSchemaIds(type => type.FullName);
+                options.HideAbpEndpoints();
             });
     }
 
