8dd8abdbf01579c8f287c21055197cde53b2267d
Mohamed Warda <mowarda.eg@gmail.com>
2025-04-18 18:41:02 +0200

    add Product service using custom service and create base class for common service logic

---
+++ src/ABPDemo.Application.Contracts/Products/Contracts/IProductsAppService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using ABPDemo.Products.Dtos;
+using Volo.Abp.Application.Dtos;
+
+namespace ABPDemo.Products.Contracts;
+
+public interface IProductsAppService
+{
+    Task<ProductDto> CreateProductAsync(CreateUpdateProductDto input);
+    Task<ProductDto> UpdateProductAsync(CreateUpdateProductDto input);
+    Task<ProductDto> GetProductAsync(int id);
+    Task<PagedResultDto<ProductDto>> GetListAsync(GetProductListDto input);
+    Task<bool> DeleteProductAsync(int id);
+}
---
+++ src/ABPDemo.Application/Bases/BaseApplicationService.cs
@@ -0,0 +1,12 @@
+﻿using Volo.Abp.Application.Services;
+
+namespace ABPDemo.Bases;
+
+// 'ApplicationService' is the base class for all application services in ABP
+public class BaseApplicationService : ApplicationService
+{
+    public BaseApplicationService()
+    {
+            
+    }
+}
