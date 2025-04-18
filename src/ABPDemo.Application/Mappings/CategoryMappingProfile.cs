7d8642c21a109d88b55f6bf6b9b8d900fa31adb8
Mohamed Warda <mowarda.eg@gmail.com>
2025-04-18 18:28:02 +0200

    Add Product and Category AutoMapper Profiles

---
+++ src/ABPDemo.Application/Mappings/CategoryMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using ABPDemo.Categories;
+using ABPDemo.Categories.Dtos;
+using AutoMapper;
+
+namespace ABPDemo.Mappings;
+
+public class CategoryMappingProfile:Profile
+{
+    public CategoryMappingProfile()
+    {
+        CreateMap<Category, CategoryDto>();
+        CreateMap<CreateUpdateCategoryDto, Category>();
+
+    }
+}
---
+++ src/ABPDemo.Application/Mappings/ProductMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using ABPDemo.Products;
+using ABPDemo.Products.Dtos;
+using AutoMapper;
+
+namespace ABPDemo.Mappings;
+
+public class ProductMappingProfile:Profile
+{
+    public ProductMappingProfile()
+    {
+        CreateMap<CreateUpdateProductDto, Product>();
+        CreateMap<Product, ProductDto>();
+    }
+}
