c7f99220814f3c007fe7fbff03ca6385428ea077
Mohamed Warda <mowarda.eg@gmail.com>
2025-04-19 15:00:18 +0200

    Dto Validation Using DataAnnotations

--- src/ABPDemo.Application.Contracts/Categories/Dtos/CreateUpdateCategoryDto.cs
+++ src/ABPDemo.Application.Contracts/Categories/Dtos/CreateUpdateCategoryDto.cs
@@ -6,8 +6,19 @@ namespace ABPDemo.Categories.Dtos;
 // Explore other base Dto entity types such as EntityDto<>, FullAuditedEntityDto<>, AuditedEntityDto<>, etc., to determine which best fits your needs.
 public class CreateUpdateCategoryDto : EntityDto<int>
 {
+    [Required]
+    [MaxLength(300)] 
     public string NameAr { get; set; }
+
+    [Required]
+    [MaxLength(300)]
     public string NameEn { get; set; }
+
+    [Required]
+    [MaxLength(1000)]
     public string DescriptionAr { get; set; }
+
+    [Required]
+    [MaxLength(1000)]
     public string DescriptionEn { get; set; }
 }
