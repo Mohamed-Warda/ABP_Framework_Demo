50ece5b6b5d8eb66774c318ec1eecdca22d0b8d9
Mohamed Warda <mowarda.eg@gmail.com>
2025-05-05 15:46:55 +0300

    add lookup service - before cashing

---
+++ src/ABPDemo.Application/Lookups/LookupsAppService.cs
@@ -0,0 +1,46 @@
+﻿using ABPDemo.Bases;
+using ABPDemo.Categories.Dtos;
+using ABPDemo.Categories;
+using AutoMapper.Internal.Mappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Caching;
+using Volo.Abp.Domain.Repositories;
+
+namespace ABPDemo.Lookups;
+public class LookupsAppService : BaseApplicationService
+{
+    #region fields
+    private readonly IRepository<Category, int> categoryRepository;
+    private readonly IDistributedCache<List<CategoryDto>> categoryCache;
+    #endregion
+
+    #region ctor
+    public LookupsAppService(
+        IRepository<Category, int> categoryRepository,
+        IDistributedCache<List<CategoryDto>> categoryCache)
+    {
+        this.categoryRepository = categoryRepository;
+        this.categoryCache = categoryCache;
+    }
+    #endregion
+
+    #region methods
+    public async Task<List<CategoryDto>> GetCategories()
+    {
+        return await GetAllCategoriesFromDbAsync();
+       
+    }
+    #endregion
+
+    #region private methods
+    private async Task<List<CategoryDto>> GetAllCategoriesFromDbAsync()
+    {
+        var categories = await categoryRepository.GetListAsync();
+        return ObjectMapper.Map<List<Category>, List<CategoryDto>>(categories);
+    }
+    #endregion
+}
